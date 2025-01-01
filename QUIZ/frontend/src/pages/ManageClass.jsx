79f8f4c9596198280648636874f6141a6ea11060
Morkmork23 <markjennuelgarcia@gmail.com>
2025-01-02 01:36:01 +0800

    npm install antd npm install @ant-design/icons

--- QUIZ/frontend/src/pages/ManageClass.jsx
+++ QUIZ/frontend/src/pages/ManageClass.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import TeacherNav from '../components/Teacher_Navbar'
 import Swal from 'sweetalert2';
 import { useUser } from '../UserContext';
+import { Button, Divider } from 'antd'
 
 export default function ManageClass() {
     const { user } = useUser();
@@ -34,15 +35,38 @@ export default function ManageClass() {
       <TeacherNav />
         {/* container 1 */}
     {classes.map((cls) => (
-      <div className='w-max'>
+      <div className='w-screen h-screen'>
         <div className='flex m-6 font-bold text-4xl'>
           <span>{cls.name}</span>  
         </div>
         
-        <div className="flex w-full h-5 m-6 bg-red-300">
+        <div className="p-4 flex flex-col w-auto h-[85%] m-6 bg-[#f1f1f1] shadow-md rounded-md">
+            <div className='flex justify-between'>
+            <span className='m-4 font-bold text-2xl'>Activity</span>
+
+            <Button 
+                className='m-4 w-auto h-auto p-2 rounded-lg shadow-md'
+                type='primary'
+            >        
+                Create a Quiz
+            </Button>
+            </div>
+
+            <div className='w-auto h-[30%] rounded-md border-2 m-2'>
+            {/* display all of the Created Activity*/}
+            </div>
+
+
+            {/* display all of the students that are enrolled */}
+            <div className='my-4 h-full'>
+            <span className='m-4 font-bold text-2xl'>Enrolled Students</span>
+
+                <div className='border-2 w-auto h-[30%] m-2'>
+                    {/* enrolled students go here */}
+                </div>
+            </div>
 
         </div>
-
       </div>))}
     </div>
   )
